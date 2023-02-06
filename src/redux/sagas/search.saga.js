3ce5db22efcd30ccc40d3f9f03c607a7786fa95f
Aman Abdirazak <amanabdirizak@icloud.com>
2023-02-06 10:14:42 -0600

    wrote axios get request in saga

---
+++ src/redux/sagas/search.saga.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+//we want to use action.payload in our function, so we need to give 'action'as a parameter
+function* searchBooks(action) {
+    try {
+        const search = action.payload;
+        console.log(search);
+        const response = yield axios({
+            method: 'GET',
+            url: `/api/search/${search}`
+        })
+    } catch {
+        
+    }
+}
+
+function* searchBooksSaga() {
+    yield takeLatest('SAGA/SEARCH_BOOKS', searchBooks);
+}
+
+export default searchBooksSaga;
