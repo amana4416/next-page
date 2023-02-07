7c48336e86de2fb791d0e6fcf93b5197425af68e
Aman Abdirazak <amanabdirizak@icloud.com>
2023-02-07 16:02:26 -0600

    sending dispatch to saga so we can get our open bookshelf populated

--- src/components/CurrentlyReadingOpen/CurrentlyReadingOpen.jsx
+++ src/components/CurrentlyReadingOpen/CurrentlyReadingOpen.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import './CurrentlyReadingOpen.css';
 
 function CurrentlyReadingOpen() {
 
+    const dispatch = useDispatch();
+
+    //using useEffect so all books marked as 'currently reading' can be loaded as soon
+    //you 'open' the bookshelf
+    useEffect(() => {
+        dispatch({
+            type: 'SAGA/FETCH_ALL_CURRENTLY_READING'
+        })
+    }, [])
+
+
     return (
         <>
-            <p>you opened the 'currently reading' bookshelf</p>
+            <h2>Currently Reading</h2>
+            <section className="currentlyReadingOpenBackground">
+                <h1>heyo</h1>
+            </section>
         </>
     )
 }
