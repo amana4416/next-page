349ee6cf8dd659c5d0535e2d367e5a193b0192fd
Aman Abdirazak <amanabdirizak@icloud.com>
2023-02-09 16:22:45 -0600

    displaying drop down menu that allow book to be added to bookshelves on menu

--- src/components/BestSellerItem/BestSellerItem.jsx
+++ src/components/BestSellerItem/BestSellerItem.jsx
@@ -2,6 +2,10 @@ import React from "react"
 import { useHistory } from 'react-router-dom';
 //mui imports
 import Paper from '@mui/material/Paper';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 function BestSellerItem({bestSeller}) {
 
@@ -28,11 +32,30 @@ function BestSellerItem({bestSeller}) {
                 </Paper>
                 <Paper 
                     elevation={3}
-                    sx={{backgroundColor: '#B7B4A2', height: '150px', width: '500px', margin: '45px', marginTop: '15px', padding: '15px', display: 'inline-block', verticalAlign: 'top'}}
+                    sx={{backgroundColor: '#B7B4A2', height: 'auto', width: '450px', margin: '45px', marginTop: '15px', padding: '15px', display: 'inline-block', verticalAlign: 'top'}}
                 >
                     <h3>{book_title}</h3>
                     <h4>Written by: {book_author}</h4>
                     <p>{book_description}</p>
+                </Paper>
+                <Paper
+                    elevation={3}
+                    sx={{backgroundColor: '#B7B4A2', height: 'auto', width: '250px', margin: '35px', marginTop: '15px', padding: '15px', display: 'inline-block', verticalAlign: 'top'}}
+                >
+                     <FormControl variant="standard" sx={{ m: 1, minWidth: 200 }}>
+                        <InputLabel id="demo-simple-select-standard-label">Select Bookshelf</InputLabel>
+                        <Select
+                        labelId="demo-simple-select-standard-label"
+                        id="demo-simple-select-standard"
+                        
+                        
+                        label="Select Bookshelf"
+                        >
+                            <MenuItem value={1}>Currently Reading</MenuItem>
+                            <MenuItem value={2}>Want To Read</MenuItem>
+                            <MenuItem value={3}>Finished</MenuItem>
+                        </Select>
+                    </FormControl>
                 </Paper>
             </div>
         </>
