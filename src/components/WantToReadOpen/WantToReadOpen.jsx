4dcb1514903e52c618bd6b0c95263a2e910483f4
Aman Abdirazak <amanabdirizak@icloud.com>
2023-02-09 08:38:47 -0600

    sending saga dispatch to retrieve all books marked as want to read

--- src/components/WantToReadOpen/WantToReadOpen.jsx
+++ src/components/WantToReadOpen/WantToReadOpen.jsx
@@ -1,8 +1,21 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import './WantToReadOpen.css';
+//mui imports
+import Paper from '@mui/material/Paper';
 
 function WantToReadOpen() {
 
+    const dispatch = useDispatch();
+
+
+    //we're going to use useEffect in this component because we want the bookshelf to populate with
+    //books marked as 'want to read' as soon as the profile page opens
+    useEffect(() => {
+        dispatch({
+            type: 'SAGA/FETCH_WANT_TO_READ'
+        })
+    }, [])
 
     return (
         <>
